220137e357c2c42eae3ce21b9fc0a2277c5b81e4
seimenjane <seimenjane@126.com>
2015-10-20 13:44:59 +0800

    input
    config project

---
+++ config/setupWindow.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace config
+{
+    partial class setupWindow
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.diskTest = new System.Windows.Forms.Button();
+            this.diskDetail = new System.Windows.Forms.Label();
+            this.diskSelector = new System.Windows.Forms.ComboBox();
+            this.okButton = new System.Windows.Forms.Button();
+            this.ipTest = new System.Windows.Forms.Button();
+            this.ipGroup = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ipInput = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.ipGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.diskTest);
+            this.groupBox1.Controls.Add(this.diskDetail);
+            this.groupBox1.Controls.Add(this.diskSelector);
+            this.groupBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(298, 264);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "数据盘设置";
+            // 
+            // diskTest
+            // 
+            this.diskTest.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.diskTest.Location = new System.Drawing.Point(150, 38);
+            this.diskTest.Name = "diskTest";
+            this.diskTest.Size = new System.Drawing.Size(75, 23);
+            this.diskTest.TabIndex = 2;
+            this.diskTest.Text = "测试";
+            this.diskTest.UseVisualStyleBackColor = true;
+            this.diskTest.Click += new System.EventHandler(this.diskTest_Click);
+            // 
+            // diskDetail
+            // 
+            this.diskDetail.AutoSize = true;
+            this.diskDetail.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.diskDetail.Location = new System.Drawing.Point(6, 115);
+            this.diskDetail.Name = "diskDetail";
+            this.diskDetail.Size = new System.Drawing.Size(0, 16);
+            this.diskDetail.TabIndex = 1;
+            // 
+            // diskSelector
+            // 
+            this.diskSelector.FormattingEnabled = true;
+            this.diskSelector.Items.AddRange(new object[] {
+            "C",
+            "D",
+            "E",
+            "F",
+            "G",
+            "H"});
+            this.diskSelector.Location = new System.Drawing.Point(6, 37);
+            this.diskSelector.Name = "diskSelector";
+            this.diskSelector.Size = new System.Drawing.Size(113, 24);
+            this.diskSelector.TabIndex = 0;
+            this.diskSelector.SelectedIndexChanged += new System.EventHandler(this.diskSelector_SelectedIndexChanged);
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(480, 253);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 2;
+            this.okButton.Text = "保存";
+            this.okButton.UseVisualStyleBackColor = true;
+            this.okButton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // ipTest
+            // 
+            this.ipTest.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ipTest.Location = new System.Drawing.Point(64, 115);
+            this.ipTest.Name = "ipTest";
+            this.ipTest.Size = new System.Drawing.Size(75, 23);
+            this.ipTest.TabIndex = 3;
+            this.ipTest.Text = "测试";
+            this.ipTest.UseVisualStyleBackColor = true;
+            this.ipTest.Click += new System.EventHandler(this.ipTest_Click);
+            // 
+            // ipGroup
+            // 
+            this.ipGroup.Controls.Add(this.label1);
+            this.ipGroup.Controls.Add(this.ipTest);
+            this.ipGroup.Controls.Add(this.ipInput);
+            this.ipGroup.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ipGroup.Location = new System.Drawing.Point(355, 12);
+            this.ipGroup.Name = "ipGroup";
+            this.ipGroup.Size = new System.Drawing.Size(200, 162);
+            this.ipGroup.TabIndex = 4;
+            this.ipGroup.TabStop = false;
+            this.ipGroup.Text = "教师机地址";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(6, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "输入教师机的ip地址";
+            // 
+            // ipInput
+            // 
+            this.ipInput.Location = new System.Drawing.Point(9, 67);
+            this.ipInput.Name = "ipInput";
+            this.ipInput.Size = new System.Drawing.Size(142, 26);
+            this.ipInput.TabIndex = 0;
+            // 
+            // setupWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 300);
+            this.Controls.Add(this.ipGroup);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "setupWindow";
+            this.Text = "设置";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ipGroup.ResumeLayout(false);
+            this.ipGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label diskDetail;
+        private System.Windows.Forms.ComboBox diskSelector;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button ipTest;
+        private System.Windows.Forms.GroupBox ipGroup;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ipInput;
+        private System.Windows.Forms.Button diskTest;
+    }
+}
+
---
+++ config/setupWindow.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security;
+using System.Configuration;
+using System.Net;
+using System.Net.Sockets;
+using System.ServiceModel;
+
+
+namespace config
+{
+    public partial class setupWindow : Form
+    {
+        private string myDriver;
+
+        public setupWindow()
+        {
+            InitializeComponent();
+            if (findIP() != "")
+            {
+                ipInput.Text = findIP();
+            }
+        }
+
+        private void setDisk()
+        {
+            DriveInfo diskInfo;
+            string diskName = this.diskSelector.Text;
+            string detail = "";
+            StringBuilder sb = new StringBuilder();
+            if (diskName == "")
+                return;
+            else
+            {
+                try { diskInfo = new DriveInfo(diskName);   }
+                catch(ArgumentException)
+                {
+                    MessageBox.Show("盘符错误！");
+                    return;
+                }
+                            
+                try
+                {
+                    sb.Append("磁盘类型：");
+                    if(diskInfo.DriveType.ToString() == "Fixed")
+                        sb.Append("硬盘");
+                    else if(diskInfo.DriveType.ToString() == "Removable")
+                        sb.Append("移动磁盘");
+                    else
+                        sb.Append(diskInfo.DriveType.ToString());
+                    sb.AppendLine();
+                    sb.Append("文件格式：");
+                    sb.Append(diskInfo.DriveFormat);
+                    sb.AppendLine();
+                    sb.Append("剩余容量：");
+                    long freeSpace = diskInfo.TotalFreeSpace / 1024 / 1024;  //可能会除法溢出
+                    sb.Append(freeSpace.ToString());
+                    sb.Append("MB");
+                    detail = sb.ToString();
+                    this.myDriver = diskInfo.Name;
+                } 
+                catch(DriveNotFoundException)
+                {
+                    detail = "驱动器不存在,\r\n请重新选择一个有效的磁盘驱动器。";
+                    this.myDriver = "";
+                    return;
+                }
+                catch (IOException)
+                {
+                    detail = "无效的驱动器,\r\n请重新选择一个有效的磁盘驱动器。";
+                    this.myDriver = "";
+                    return;
+                }
+                finally
+                {
+                    this.diskDetail.Text = detail;
+                }
+            }
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            saveConfig("dataDisk",this.myDriver);
+            saveConfig("teacherIP", this.ipInput.Text);
+            StringBuilder sb = new StringBuilder();
+            sb.Append("保存成功：\t\n");
+            sb.Append("数据磁盘：");
+            sb.Append(this.myDriver + "\t\n");
+            sb.Append("教师机地址：");
+            sb.Append(this.ipInput.Text);
+            MessageBox.Show(sb.ToString());
+        }//保存按钮
+
+        private void diskTest_Click(object sender, EventArgs e)
+        {
+            if (this.myDriver == "" || this.myDriver == null)
+            {
+                MessageBox.Show("先选择一个有效的磁盘驱动器");
+                return;
+            }
+            else
+            {
+                DirectoryInfo df = new DirectoryInfo(this.myDriver + "cn.sh.edu.fdkjy.seimen");
+               
+                try
+                {
+                    if (df.Exists)
+                        df.Delete();
+                    df.Create();
+                    df.Delete();
+                    MessageBox.Show("测试成功");
+                }
+                 catch (SecurityException)
+                {
+                    MessageBox.Show("权限测试出错");
+                    return;
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("读写测试出错");
+                    return;
+                }
+            }
+        }//磁盘测试按钮
+
+        private void diskSelector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            setDisk();
+        }
+
+        static void saveConfig(string setKey, string setValue)
+        {
+            try
+            {
+                string confPath = System.Windows.Forms.Application.StartupPath + @"\App.config";
+                
+                System.Configuration.Configuration config =
+                    ConfigurationManager.OpenExeConfiguration(confPath);
+                bool exist = false;
+                foreach (string key in config.AppSettings.Settings.AllKeys)
+                {
+                    if (key == setKey)
+                    {
+                        exist = true;
+                    }
+                }
+                if (exist)
+                {
+                    config.AppSettings.Settings.Remove(setKey);
+                }
+                config.AppSettings.Settings.Add(setKey, setValue);
+                config.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection("appSettings");
+            }
+            catch (ConfigurationErrorsException err)
+            {
+                MessageBox.Show(err.ToString());
+                return;
+            }
+            catch (Exception)
+            { MessageBox.Show("保存配置出错"); return; }
+        }
+
+        private void ipTest_Click(object sender, EventArgs e)//地址测试按钮
+        {
+            using (ChannelFactory<service.ItestServiceContract> channelFactory = new ChannelFactory<service.ItestServiceContract>(new WSHttpBinding(), "http://10.29.114.251:8000/testServices"))
+            {
+                service.ItestServiceContract proxy = channelFactory.CreateChannel();
+                using (proxy as IDisposable)
+                {
+                    MessageBox.Show(proxy.shakeHand("test").ToString());
+                }
+            }
+        }
+
+        private string findIP()
+        {
+            try
+            {
+                var ipaddress = Dns.GetHostAddresses("teacher");
+                foreach (IPAddress ip in ipaddress)
+                {
+                    if (ip.AddressFamily == AddressFamily.InterNetwork)
+                        return ip.ToString();
+                }
+                return "";
+            }
+            catch (SocketException)
+            {
+                return "";
+            }
+        }
+    }
+}
